4bdc6f3cbc43c140e3f06ea3e2d1a9b9a40ca621
Anthony Diaz <diazanthony32@gmail.com>
2014-09-30 17:11:04 -0700

    BlogReader Update
    Made the array compatible and print out with the listview

--- app/src/main/java/anroid/diaza/blogreader/BlogActivity.java
+++ app/src/main/java/anroid/diaza/blogreader/BlogActivity.java
@@ -2,6 +2,7 @@ package anroid.diaza.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -17,12 +18,24 @@ public class BlogActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
 
+        String[] arrayStrings = new String[] {
+
+                "Blog Post 1",
+                "Blog Post 2",
+                "Blog Post 3",
+                "Blog Post 4"
+        };
+
         // connecting to the ids in the activity
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         listView = (ListView)findViewById(R.id.listView);
 
         // If listView is Empty it sets the view to empty
         listView.setEmptyView(progressBar);
+
+        // makes it so our array is able to be used in our listView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+        listView.setAdapter(adapter);
     }
 
 }
