e1cbc9f2b97b06d9645b11ccaf101fa37a0049df
Anthony Diaz <diazanthony32@gmail.com>
2014-10-01 10:47:26 -0700

    BlogPost Update
    Made a new Java file called BlogPost

---
+++ app/src/main/java/anroid/diaza/blogreader/BlogPost.java
@@ -0,0 +1,12 @@
+package anroid.diaza.blogreader;
+
+public class BlogPost {
+
+    public final String title;
+    public final String url;
+
+    public BlogPost(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+}
