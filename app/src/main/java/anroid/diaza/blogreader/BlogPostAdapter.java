66a6fa523c3d9f9c871e66c437a7556d0cbde4ea
Anthony Diaz <diazanthony32@gmail.com>
2014-10-20 14:42:28 -0700

    BlogReader Update
    Milestone 29

--- app/src/main/java/anroid/diaza/blogreader/BlogPostAdapter.java
+++ app/src/main/java/anroid/diaza/blogreader/BlogPostAdapter.java
@@ -1,15 +1,19 @@
 package anroid.diaza.blogreader;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class BlogPostAdapter extends ArrayAdapter<BlogPost>{
     public BlogPostAdapter(Context context, ArrayList<BlogPost> posts) {
         super(context, 0, posts);
     }
 
-    
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        
+    }
 }
