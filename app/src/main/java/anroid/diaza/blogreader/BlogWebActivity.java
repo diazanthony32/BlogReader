707a38286e170bf8b22a83dd9df451ad0df7c909
Anthony Diaz <diazanthony32@gmail.com>
2014-10-22 14:35:24 -0700

    BlogReader Update #36
    Milestone 36

---
+++ app/src/main/java/anroid/diaza/blogreader/BlogWebActivity.java
@@ -0,0 +1,12 @@
+package anroid.diaza.blogreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlogWebActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_web);
+    }
+}
