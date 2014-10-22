a0e7130419b0668a1bd40088d83a3903f500c7f9
Anthony Diaz <diazanthony32@gmail.com>
2014-10-22 14:22:51 -0700

    BlogReader Update #34
    Milestone #34

--- app/src/main/java/anroid/diaza/blogreader/BlogActivity.java
+++ app/src/main/java/anroid/diaza/blogreader/BlogActivity.java
@@ -2,6 +2,9 @@ package anroid.diaza.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -24,6 +27,13 @@ public class BlogActivity extends Activity {
 
         // If listView is Empty it sets the view to empty
         listView.setEmptyView(progressBar);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("BlogActivity", "Title: " + BlogPostParser.get().posts.get(position).title);
+            }
+        });
 
         new BlogPostTask().execute(this);
     }
--- app/src/main/java/anroid/diaza/blogreader/BlogPostTask.java
+++ app/src/main/java/anroid/diaza/blogreader/BlogPostTask.java
@@ -22,7 +22,7 @@ public class BlogPostTask extends AsyncTask <Activity, Void, JSONObject>{
         JSONObject jsonObject = null;
 
         try {
-            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=20");
 
             HttpURLConnection connection = (HttpURLConnection)blogFeedUrl.openConnection();
             connection.connect();
