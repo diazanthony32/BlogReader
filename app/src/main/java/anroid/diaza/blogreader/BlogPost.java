ab872c05f111de55e5a2f820ee5bd1200c0b9031
Anthony Diaz <diazanthony32@gmail.com>
2014-10-23 15:02:42 -0700

    BlogReader Intermediate Update #3
    Intermediate milestone #3

--- app/src/main/java/anroid/diaza/blogreader/BlogPost.java
+++ app/src/main/java/anroid/diaza/blogreader/BlogPost.java
@@ -4,11 +4,17 @@ public class BlogPost {
 
     public final String title;
     public final String url;
+    public final String date;
+    public final String author;
+    public final String image;
 
-    //sets the variables for BlogPost;
-    public BlogPost(String title, String url) {
+    //sets the variables of BlogPost
+    public BlogPost(String title, String url, String date, String author, String image) {
         this.title = title;
         this.url = url;
+        this.date = date;
+        this.author = author;
+        this.image = image;
     }
 
 }
